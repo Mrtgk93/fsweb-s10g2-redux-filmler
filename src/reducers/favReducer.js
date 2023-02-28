63804230ce17a46cd77e0632e5c6f836ab8c578c
Mrtgk93 <mrtgk93@hotmail.com>
2023-03-01 00:26:33 +0300

    film silme ve ekleme işlemine kadar yapabildim

---
+++ src/reducers/favReducer.js
@@ -0,0 +1,11 @@
+export const initialState = {
+  favorites: [],
+  displayFavorites: false,
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+export default reducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
-import movieReducer from './movieReducer';
+import movieReducer from "./movieReducer";
+import favReducer from "./favReducer";
 
+const rootReducer = combineReducers({
+  movie: movieReducer,
+  fav: favReducer,
+});
 export default movieReducer;
